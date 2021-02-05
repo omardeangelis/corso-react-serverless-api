5474ab3898bfc94889e474e7cc5d2cc6cad4493f
Omar De Angelis <omar.hpvfilm@gmail.com>
2021-02-05 23:32:09 +0100

    Cors handled

--- src/holiday.js
+++ src/holiday.js
@@ -3,7 +3,6 @@ const serverless = require("serverless-http");
 const app = express();
 
 const holiday = require("./routes/holiday");
-
 app.use("/.netlify/functions/holiday", holiday);
 
 module.exports.handler = serverless(app);
--- src/routes/holiday.js
+++ src/routes/holiday.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const cors = require("cors");
+
 const data = require("../_data/holiday");
 
 const router = express.Router();
 
-router.route("/").get((req, res) => {
+router.route("/").get(cors(), (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
   res.status(200).json({
     success: true,
     data,
