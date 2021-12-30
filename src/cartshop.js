9b3f8464ce6497f6a7e5a821bbab8b7f29a1cee0
Omar De Angelis <omar.hpvfilm@gmail.com>
2021-12-30 18:57:36 +0100

    add express rate limit

--- src/cartshop.js
+++ src/cartshop.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const rateLimit = require("express-rate-limit");
 const app = express();
-
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 15 minutes
+  max: 200, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  message:
+    "Limite di 200 richieste per minuto superato. Attendi 1 minuto e riprova",
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+app.use(limiter);
 const cartshop = require("./routes/cartshop");
 app.use("/.netlify/functions/cartshop", cartshop);
 
